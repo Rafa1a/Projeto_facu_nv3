f5f6491b25bb771ca711676ac9109299499b724f
Rafa1a <110539932+Rafa1a@users.noreply.github.com>
2023-02-21 17:40:30 -0300

    nova tentativa 2

--- livraria_on/next.config.js
+++ livraria_on/next.config.js
@@ -1,3 +1,10 @@
+const withTM = require('next-transpile-modules')(['bootstrap']);
+
+module.exports = withTM({
+  // configurações do Next.js aqui...
+  
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
