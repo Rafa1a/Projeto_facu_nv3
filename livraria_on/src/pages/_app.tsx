c68441d41891f72958bb2c332a77ecdbba452fcc
Rafa1a <110539932+Rafa1a@users.noreply.github.com>
2023-02-21 17:18:53 -0300

    up deploy
    type of window

--- livraria_on/src/pages/_app.tsx
+++ livraria_on/src/pages/_app.tsx
@@ -2,7 +2,13 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
+import { useEffect } from "react";
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      require("bootstrap/dist/js/bootstrap.bundle.min.js");
+    }
+  }, []);
+
   return <Component {...pageProps} />
 }
