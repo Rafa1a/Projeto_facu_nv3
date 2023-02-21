d5fa6cab6b773b6d9450c9ad589e7e73d469c6c1
Rafa1a <110539932+Rafa1a@users.noreply.github.com>
2023-02-21 17:52:10 -0300

    up nova tentativa 3

--- livraria_on/next.config.js
+++ livraria_on/next.config.js
@@ -1,9 +1,3 @@
-const withTM = require('next-transpile-modules')(['bootstrap']);
-
-module.exports = withTM({
-  // configurações do Next.js aqui...
-  
-});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
