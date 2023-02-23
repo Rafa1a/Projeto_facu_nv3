9878547b72494223c9dc54bf6c79643e54720708
Rafa1a <110539932+Rafa1a@users.noreply.github.com>
2023-02-22 22:07:18 -0300

    up envio fetch

--- livraria_on/src/pages/controle/ControleEditora.tsx
+++ livraria_on/src/pages/controle/ControleEditora.tsx
@@ -1,19 +1,16 @@
 import Editora from '../modelo/Editora';
 
-const editoras: Array<Editora> = [
-  { codEditora: 1, nome: 'Editora 1' },
-  { codEditora: 2, nome: 'Editora 2' },
-  { codEditora: 3, nome: 'Editora 3' },
-];
-
-export function getEditoras(): Array<Editora> {
-  return editoras;
+export async function getEditoras(): Promise<Array<Editora>> {
+  const response = await fetch('http://localhost:3000/api/editoras');
+  const data = await response.json();
+  return data;
 }
 
-export function getNomeEditora(codEditora: number): string | undefined {
-  const editoraEncontrada = editoras.find((editora) => editora.codEditora === codEditora);
-  if (editoraEncontrada) {
-    return editoraEncontrada.nome;
+export async function getNomeEditora(codEditora: number): Promise<string | undefined> {
+  const response = await fetch(`http://localhost:3000/api/editoras/${codEditora}`);
+  const data = await response.json();
+  if (data) {
+    return data.nome;
   }
   return undefined;
 }
