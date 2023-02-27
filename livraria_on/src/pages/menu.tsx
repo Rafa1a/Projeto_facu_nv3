de2639affc2b7c8c9a8b76e1244e058f2b769379
Rafa1a <110539932+Rafa1a@users.noreply.github.com>
2023-02-27 19:53:38 -0300

    corrigindo menu e adicionando exemplos de livros

--- livraria_on/src/pages/menu.tsx
+++ livraria_on/src/pages/menu.tsx
@@ -7,7 +7,10 @@ export const Menu: React.FC = () => {
         <Link href="/" className="navbar-brand">
          Loja Next
         </Link>
-        <div className="collapse navbar-collapse">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <Link href="/" className="nav-link">
